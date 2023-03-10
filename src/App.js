f017c4c50732d66dd6767a4661fde1df720fdaa7
Marison <marisonmunoz@gmail.com>
2023-03-10 15:08:33 -0500

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Marison Munoz and is{" "}
-        <a
-          href="https://github.com/mariscript/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://marison.me" target="_blank">
+            Marison Munoz
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/mariscript/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
