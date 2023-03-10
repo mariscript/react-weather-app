41eb77a5360744e14d38a82a7d4a2c1eb50cb978
Marison <marisonmunoz@gmail.com>
2023-03-10 14:07:43 -0500

    added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Marison Munoz and is{" "}
+        <a
+          href="https://github.com/mariscript/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
